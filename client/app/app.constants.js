190f5566f5ab7da170323e13cdb2085d1784dfda
rolox05 <r.sobisch@globallogic.com>
2016-09-29 14:26:34 -0300

    delete

--- client/app/app.constants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import angular from 'angular';
-
-export default angular.module('webScanFacturasApp.constants', [])
-  .constant('appConfig', require('../../server/config/environment/shared'))
-  .name;
